bea71370dc83342d47fe965654781b198b27eefe
daniel-sciome <daniel.svoboda@sciome.com>
2020-07-17 10:27:24 -0400

    Update app.R

--- app.R
+++ app.R
@@ -1,3 +1,9 @@
 # App to view TG-GATES Affymetrix Intensities as a distribution (box plot) across 28 day average pathology severity scores.
 # Box plots are rendered based on the selected duration(s), and gene(s).
 
+library(shiny)
+
+ui <- 
+
+server <- function(input, output, session) {
+  }
